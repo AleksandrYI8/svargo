16ef2fcc643c15100bf7326cf89172b8ca828853
Yunus <ibadovunus55@gmail.com>
2024-05-29 10:23:50 +0500

    fixed

--- main.js
+++ main.js
@@ -2,16 +2,24 @@ let modal = document.querySelector(".modal_dialog")
 let modal_close = document.querySelector(".close_btn")
 let menu = document.querySelector(".menu")
 let container = document.querySelector(".container")
-let header = document.querySelector(".header_box")
+let header = document.querySelector(".header")
+let line = document.querySelector(".line_header")
+let header_box = document.querySelector(".header_box")
 let body = document.body
+
+
 
 menu.onclick = () => {
   modal.classList.add("show")
   modal.classList.remove("hide")
-
+  
+  let rect = header_box.offsetTop 
+  
+  modal.style.top = rect + "px"
   body.classList.add("overflow")
   container.classList.add("opacity")
   header.classList.add("opacity")
+  line.classList.add("opacity")
   
 }
 
@@ -23,5 +31,7 @@ modal_close.onclick = () => {
   body.classList.remove("overflow")
   container.classList.remove("opacity")
   header.classList.remove("opacity")
+  line.classList.remove("opacity")
+
 }
 
